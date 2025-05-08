87a6c6cbb44be072c243a9edc13de7c72ee84f57
Lisi-Pedrassoli <lisi1809.net@gmail.com>
2025-05-07 21:58:51 -0300

    teste commit pelo intelij 2

--- src/main/java/com/manhattan/demo/Services/Sale/SaleService.java
+++ src/main/java/com/manhattan/demo/Services/Sale/SaleService.java
@@ -62,7 +62,7 @@ public class SaleService {
                 .map(saleProductEntity -> saleProductEntity.getValor() * saleProductEntity.getQuantidadeSaida())
                 .reduce(0.0f, Float::sum));
 
-        System.out.println("Teste branch ttwste");
+        System.out.println("Teste branch commit intelij 3");
 
         return this.repository.save(sale);
     }
