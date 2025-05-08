4b57d349762992c23635ac947bcc53998d6e1f59
Lisi-Pedrassoli <lisi1809.net@gmail.com>
2025-05-08 19:53:27 -0300

    Ativei o Delet novamente, mas ainda não funciona

--- src/main/java/com/manhattan/demo/Services/Sale/SaleService.java
+++ src/main/java/com/manhattan/demo/Services/Sale/SaleService.java
@@ -62,8 +62,6 @@ public class SaleService {
                 .map(saleProductEntity -> saleProductEntity.getValor() * saleProductEntity.getQuantidadeSaida())
                 .reduce(0.0f, Float::sum));
 
-        System.out.println("Teste branch commit intelij 4");
-
         return this.repository.save(sale);
     }
 
