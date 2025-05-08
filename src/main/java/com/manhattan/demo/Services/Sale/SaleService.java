50d6b11af6763fc8780c2ce195cbb2bf27b4b11c
Lisi-Pedrassoli <lisi1809.net@gmail.com>
2025-05-07 21:54:54 -0300

    feat: criada classe sale service

--- src/main/java/com/manhattan/demo/Services/Sale/SaleService.java
+++ src/main/java/com/manhattan/demo/Services/Sale/SaleService.java
@@ -62,7 +62,7 @@ public class SaleService {
                 .map(saleProductEntity -> saleProductEntity.getValor() * saleProductEntity.getQuantidadeSaida())
                 .reduce(0.0f, Float::sum));
 
-        System.out.println("Teste branch");
+        System.out.println("Teste branch ttwste");
 
         return this.repository.save(sale);
     }
